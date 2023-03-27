9db4f0c910004f5bb7bb314945a7de07359d273e
MusanjeLawrence <musanjelawrence6@gmail.com>
2023-03-27 19:49:06 +0300

    created models for  task table

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import TaskList, DetailView
 
 
 urlpatterns = [
-    path('', views.taskList, name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='tasks'),
+
 ]
